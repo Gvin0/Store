d6ecfa8a0c988cc922106dd405afaac660a7e0e1
Giorgi Tkachenko <GioGerrard@gmail.com>
2019-06-17 22:05:37 +0400

    Minor changes

---
+++ Store.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Store.Domain.Interfaces {
+   public interface IBaseRepository<T> where T : class {
+        T Fetch(int id);
+        IEnumerable<T> Set();
+        void Save(T entity);
+        void Delete(int id);
+        void Delete(T entity);
+        void Save(DbSet<T> set, T entity);
+        void Delete(DbSet<T> set, T entity);
+    }
+}
